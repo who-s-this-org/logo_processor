269bc7ee67e69a017ad4b71a7a91333c0534acdf
Arpit Jain <me.arpitjain09@gmail.com>
2026-03-13 23:24:06 +0530

    fix move script

--- scripts/move.sh
+++ scripts/move.sh
@@ -3,14 +3,38 @@ for dir in {A..Z}; do
 done
 mkdir -p 0
 
+move_file() {
+  local file="$1"
+  local dest_dir="$2"
+  local base="${file%.*}"
+  local ext="${file##*.}"
+
+  if [[ "$base" == "$file" ]]; then
+    ext=""
+  fi
+
+  local target="$file"
+  local counter=1
+  while [[ -e "$dest_dir/$target" ]]; do
+    if [[ -n "$ext" ]]; then
+      target="${base}_${counter}.${ext}"
+    else
+      target="${base}_${counter}"
+    fi
+    ((counter))
+  done
+
+  mv "$file" "$dest_dir/$target"
+}
+
 # Move files into directories named after the first letter
 for file in *; do
   if [[ -f "$file" ]]; then
     first_char=$(echo "$file" | head -c 1 | tr '[:lower:]' '[:upper:]')
     if [[ "$first_char" =~ [A-Z] ]]; then
-      mv -n "$file" "$first_char/"
+      move_file "$file" "$first_char"
     elif [[ "$file" =~ ^[0-9] ]]; then
-      mv -n "$file" "0/"
+      move_file "$file" "0"
     fi
   fi
 done
